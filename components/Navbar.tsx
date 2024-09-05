d002c1d453b0ae34dcfce624dc182499441385be
eliud <eliudobure@gmail.com>
2024-09-05 16:18:52 +0300

    All done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,21 +22,33 @@ const NavLink = styled(Button)({
 
 const Navbar: React.FC = () => (
   <AppBar 
-    position="static" 
+    position="fixed"  // Set to fixed
     style={{ 
+      top: 0,
+      left: 0,
+      right: 0,
       backgroundColor: 'transparent', 
       padding: '10px 0', 
-      boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.5)', // Adds the shadow below the navbar
-      borderBottom: '1px solid rgba(255, 255, 255, 0.1)', // Border to further separate the navbar
+      boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.5)',
+      borderBottom: '1px solid rgba(255, 255, 255, 0.1)',
+      zIndex: 1300,     // Ensure it stays above other content
     }}
   >
     <Toolbar style={{ justifyContent: 'space-between' }}>
       <Logo>Eliud</Logo>
       <Box>
-        <NavLink component={Link} href="/">Home</NavLink>
-        <NavLink component={Link} href="/about">About</NavLink>
-        <NavLink component={Link} href="/portfolio">Portfolio</NavLink>
-        <NavLink component={Link} href="/contact">Contact</NavLink>
+        <Link href="/" passHref>
+          <NavLink>Home</NavLink>
+        </Link>
+        <Link href="/about" passHref>
+          <NavLink>About</NavLink>
+        </Link>
+        <Link href="/portfolio" passHref>
+          <NavLink>Portfolio</NavLink>
+        </Link>
+        <Link href="/contact" passHref>
+          <NavLink>Contact</NavLink>
+        </Link>
       </Box>
     </Toolbar>
   </AppBar>
