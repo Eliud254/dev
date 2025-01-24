6da4796fede98913d0ddc97035e8943e1b49d522
Eliud254 <eliudobure@gmail.com>
2025-01-25 00:10:37 +0300

    Dist created

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        
+      ],
+    },
+  },
 })
