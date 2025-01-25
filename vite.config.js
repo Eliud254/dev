5af301bbf25a3533e36dd161bbccc9eca0ef8daf
Eliud254 <eliudobure@gmail.com>
2025-01-25 09:26:45 +0300

    Another commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        
-      ],
-    },
-  },
 })
